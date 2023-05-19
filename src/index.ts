614d24f7c246ecf02370f1ebb2f2b388d9f262fc
labrac <raquelortegalabrac@hotmail.com>
2023-05-19 19:41:54 +0200

    Add main.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Main } from './components/main';
 
-new Header('#app', '');
+new Header('#app', 'Header');
+new Main('#app', '');
+new Footer('#app', 'Footer');
