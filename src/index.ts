056ed1fdca7e1efe72caf035389320385586daa6
labrac <raquelortegalabrac@hotmail.com>
2023-05-21 23:39:43 +0200

    Update pokemon-api and pokemon-list

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 /* eslint-disable no-new */
-
-import { Header } from './components/header';
-import { Main } from './components/main';
-import { Card } from './components/card';
-import { Footer } from './components/footer';
+import { Header } from './component/header';
+import { Main } from './component/main';
+import { PokemonList } from './component/pokemon-list';
+import './styles.css';
 
 new Header('#app');
 new Main('#app');
-new Card('main');
-new Footer('#app', '');
+new PokemonList('main');
